f5270dba4a38ee32b0d92dc40949a5264e1046f9
kiadso <genelle@eyedarts.net>
2019-03-19 00:05:23 -0400

    got a little closer by using a <div></div>, thanks stackoverflow

--- NyckelharpaOrigins.R
+++ NyckelharpaOrigins.R
@@ -7,29 +7,29 @@ library(mapview)
 
 x <- read_xlsx("NyckelharpaHistoryData.xlsx")
 
-leaflet(x) %>%
-  setView(lng = 16.5, lat = 57, zoom = 3.5) %>% 
-  addProviderTiles("Esri.NatGeoWorldMap") %>%
-  addCircleMarkers(
-    lng = ~Longitude, 
-    lat = ~Latitude,
-    popup = paste0(popupImage(x$Localimage, src = "local", width = 75, embed = TRUE),
-                  "<br><b>Dates Back To ",
-                   x$DatesBackTo,
-                   "</b><br>",
-                   x$Description))
+description <- paste0("<br><b>Dates Back To ",
+                    x$DatesBackTo,
+                    "</b><br>",
+                    x$Description)
+
+image75 <- popupImage(x$Localimage, src = "local", width = 100)
 
 leaflet(x) %>%
-  setView(lng = 16.5, lat = 57, zoom = 3.5) %>% 
+  setView(lng = 16.5, lat = 57, zoom = 3.5) %>%
   addProviderTiles("Esri.NatGeoWorldMap") %>%
   addCircleMarkers(
-    lng = ~Longitude, 
-    lat = ~Latitude,
-    popup = c(popupImage(image75), #not working
-      paste0(
-        "<br><b>Dates Back To ",
-        x$DatesBackTo,
-        "</b><br>",
-        x$Description
-      )
-    ))
+    lng = ~ Longitude,
+    lat = ~ Latitude,
+    popup = paste0(
+      "<div><h3><b>Dates Back To: ",
+      x$DatesBackTo,
+      "</b></h3>",
+      x$Description,
+      "</br><img src=\"",
+      image75,
+      "\"></div>"
+    )
+  )
+  
+
+
