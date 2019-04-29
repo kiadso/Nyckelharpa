ceb035517ac72077ab7364588031524800799d3d
Genelle Denzin <genelle@eyedarts.net>
2019-04-29 07:15:33 -0400

    created a Global.R file because the libraries weren't loading and it just seems cleaner.

---
+++ Nyckelharpa_Origins/global.R
@@ -0,0 +1,40 @@
+library(shiny)
+library(leaflet)
+library(dplyr)
+library(htmlwidgets)
+library(rvest)
+library(readxl)
+library(mapview)
+library(here)
+
+x <- read_xlsx(here("NyckelharpaHistoryData.xlsx")) %>%
+  arrange(DatesBackTo)
+
+image75 <- popupImage(x$Localimage,
+                      src = "local",
+                      width = "200px",
+                      height = "100%")
+
+origins <- leaflet(x) %>%
+  setView(lng = 16.5, lat = 57, zoom = 3.5) %>%
+  addProviderTiles("Esri.NatGeoWorldMap") %>%
+  addCircleMarkers(
+    lng = ~ Longitude,
+    lat = ~ Latitude,
+    popup =
+      paste0(
+        "<div style=\"background-color: #fff; padding: 8px;\"><h3><b>Dates Back To ",
+        x$DatesBackTo,
+        "</b></h3><div>",
+        x$Description,
+        "</div><br>",
+        image75,
+        "</div><div style=\"background-color: #fff; padding: 8px; \"><small>Photo:",
+        x$Attribution,
+        "</small></div>"
+      ),
+    popupOptions = popupOptions(keepInView = TRUE),
+    group = x$DatesBackTo
+  ) %>%
+  addLayersControl(overlayGroups = x$DatesBackTo,
+                   options = layersControlOptions(collapsed = FALSE))
