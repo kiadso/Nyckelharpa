81eb562db34ad2e594f5cf05cc5467bc2abd9352
Genelle Denzin <genelle@eyedarts.net>
2019-04-29 07:14:10 -0400

    added the necessary arguments to the output function

--- Nyckelharpa_Origins/ui.R
+++ Nyckelharpa_Origins/ui.R
@@ -4,6 +4,9 @@ library(leaflet)
 shinyUI(fluidPage(
   titlePanel("Nyckelharpa Origins"),
       mainPanel(
-       shinyWidgetOutput("originsMap")
+       shinyWidgetOutput(outputId = "map", 
+                         name = "originsMap",
+                         width = "100%",
+                         height = "100%")
     )
 ))
