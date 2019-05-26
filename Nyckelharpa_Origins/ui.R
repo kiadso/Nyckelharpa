6e2a150ec736c161f9e2be4184b4e19d7dbac024
Genelle Denzin <genelle@eyedarts.net>
2019-05-25 23:46:10 -0400

    added a footer so I could cite my packages

--- Nyckelharpa_Origins/ui.R
+++ Nyckelharpa_Origins/ui.R
@@ -1,9 +1,36 @@
-
-fluidPage(
-  titlePanel("Nyckelharpa Origins"),
-  mainPanel(
-    leafletOutput(
-      outputId = "originsMap"
-    )
+tagList(
+  fluidPage(titlePanel("Nyckelharpa Origins"),
+            mainPanel(
+              leafletOutput(
+                outputId = "originsMap",
+                height = 800,
+                width = 600
+              )
+            )),
+  tags$footer(
+    HTML(
+      "<p>Except where otherwise noted, content on this site is licensed
+      under a
+      <a href=\"https://creativecommons.org/licenses/by-sa/4.0/legalcode\">
+      Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) license</a>.
+      
+      <p>R Core Team (2019). R: A language and environment for statistical
+      computing. R Foundation for Statistical Computing, Vienna, Austria. URL
+      https://www.R-project.org/.
+      
+      <p>Winston Chang, Joe Cheng, JJ Allaire, Yihui Xie and Jonathan McPherson
+      (2019). shiny: Web Application Framework for R. R package version 1.3.2. 
+      https://CRAN.R-project.org/package=shiny
+      
+      <p>Joe Cheng, Bhaskar Karambelkar and Yihui Xie (2018). leaflet: Create
+      Interactive Web Maps with the JavaScript 'Leaflet' Library. R package
+      version 2.0.2. https://CRAN.R-project.org/package=leaflet
+      
+      <p>Tim Appelhans, Florian Detsch, Christoph Reudenbach and Stefan Woellauer
+      (2018). mapview: Interactive Viewing of Spatial Data in R. R package
+      version 2.6.3. https://CRAN.R-project.org/package=mapview"
+      
+    ),
+    style = "width:100%; color: white; padding: 10px; background-color: black"
   )
 )
