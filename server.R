189661a1089768074d8ae3dac827a4995c724f6d
Genelle Denzin <genelle@eyedarts.net>
2019-11-14 22:10:49 -0500

    moved the logic into the Server.R so the checkbox input can control which dots show that way also made the dots so that each century shares a bright color

--- server.R
+++ server.R
@@ -3,6 +3,38 @@
 
 function(input, output) {
   output$originsMap <- renderLeaflet({
-    origins
+    x <- x %>%
+      filter(Century %in% c(input$centuries))
+    
+    image75 <- popupImage(x$RemoteImage,
+                          src = "remote",
+                          width = "200px",
+                          height = "100%")
+    
+    leaflet(x) %>%
+      clearBounds() %>%
+      addProviderTiles("Esri.NatGeoWorldMap") %>%
+      addCircleMarkers(
+        lng = ~ Longitude,
+        lat = ~ Latitude,
+        radius = 3,
+        opacity = 6,
+        color = ~x$Color,
+        fillColor = ~x$Color,
+        popup =
+          paste0(
+            "<div style=\"background-color: #fff; padding: 8px;\"><h3><b>Dates Back To ",
+            x$DatesBackTo,
+            "</b></h3><div>",
+            x$Description,
+            "</div><br>",
+            image75,
+            "</div><div style=\"background-color: #fff; padding: 8px; \"><small>Photo:",
+            x$Attribution,
+            "</small></div>"
+          ),
+        popupOptions = popupOptions(keepInView = TRUE),
+        group = x$DatesBackTo
+      ) 
   })
 }
