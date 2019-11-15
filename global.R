c77a0bbf9385856e0949735b6ec3a954908cb628
Genelle Denzin <genelle@eyedarts.net>
2019-11-14 22:11:11 -0500

    added Century and Color to the dataset

--- global.R
+++ global.R
@@ -6,36 +6,30 @@ library(readxl)
 library(mapview)
 library(leafpop)
 library(here)
+library(stringr)
 #library(ggmap)
 
-x <- read_xlsx(here("NyckelharpaHistoryData.xlsx")) %>% arrange(DatesBackTo)
+x <- read_xlsx(here("NyckelharpaHistoryData.xlsx")) %>% 
+  arrange(DatesBackTo) %>%
+  mutate(Century = case_when(
+    str_starts(DatesBackTo, "12") ~ "1200's",
+    str_starts(DatesBackTo, "13") ~ "1300's",
+    str_starts(DatesBackTo, "14") ~ "1400's",
+    str_starts(DatesBackTo, "15") ~ "1500's",
+    str_starts(DatesBackTo, "16") ~ "1600's",
+    str_starts(DatesBackTo, "17") ~ "1700's",
+    str_starts(DatesBackTo, "18") ~ "1800's",
+    str_starts(DatesBackTo, "19") ~ "1900's"
+  ),
+  Color = case_when(
+    Century == "1200's" ~ "#FF0000",
+    Century == "1300's" ~ "#FF7F00",
+    Century == "1400's" ~ "#FFFF00",
+    Century == "1500's" ~ "#00FF00",
+    Century == "1600's" ~ "#0000FF",
+    Century == "1700's" ~ "#4B0082",
+    Century == "1800's" ~ "#9400D3",
+    Century == "1900's" ~ "#222222"
+  ))
 
-image75 <- popupImage(x$RemoteImage,
-                      src = "remote",
-                      width = "200px",
-                      height = "100%")
-
-origins <- leaflet(x) %>%
-  setView(lng = 16.5, lat = 57, zoom = 3.5) %>%
-  addProviderTiles("Esri.NatGeoWorldMap") %>%
-  addCircleMarkers(
-    lng = ~ Longitude,
-    lat = ~ Latitude,
-    popup =
-      paste0(
-        "<div style=\"background-color: #fff; padding: 8px;\"><h3><b>Dates Back To ",
-        x$DatesBackTo,
-        "</b></h3><div>",
-        x$Description,
-        "</div><br>",
-        image75,
-        "</div><div style=\"background-color: #fff; padding: 8px; \"><small>Photo:",
-        x$Attribution,
-        "</small></div>"
-      ),
-    popupOptions = popupOptions(keepInView = TRUE),
-    group = x$DatesBackTo
-  ) %>%
-  addLayersControl(overlayGroups = x$DatesBackTo,
-                   options = layersControlOptions(collapsed = FALSE))
 
